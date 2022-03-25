e02d961c0dce22d78019824db355979fc829ff1c
vitaly.gusev <vitaly.gusev@progforce.com>
2022-03-25 12:31:03 +0300

    release 0.21

--- main.py
+++ main.py
@@ -1,18 +1,97 @@
 
 import tensorflow as tf
+from tensorflow import keras
+from keras import layers
 from keras.models import Model
 from keras.layers import Input
-
 import pathlib
 import numpy as np
 import matplotlib.pyplot as plt
+import os
+import tensorflow_hub as hub
+import tensorflow_datasets as tfds
+import cv2
 
+
+def format_image(image, label):
+  image = tf.image.resize(image, IMAGE_SIZE) / 255.0
+  return  image, label
 
 
 
 
 
 if __name__ == '__main__':
+
+    print("Version: ", tf.__version__)
+    print("Eager mode: ", tf.executing_eagerly())
+    print("Hub version: ", hub.__version__)
+    print("GPU is", "available" if tf.config.list_physical_devices('GPU') else "NOT AVAILABLE")
+
+    module_selection = ("inception_v3", 299, 2048)  # @param ["(\"mobilenet_v2\", 224, 1280)", "(\"inception_v3\", 299, 2048)"] {type:"raw", allow-input: true}
+    handle_base, pixels, FV_SIZE = module_selection
+    MODULE_HANDLE = "https://tfhub.dev/google/tf2-preview/{}/feature_vector/4".format(handle_base)
+    IMAGE_SIZE = (pixels, pixels)
+    BATCH_SIZE = 32
+    print("Using {} with input size {} and output dimension {}".format(MODULE_HANDLE, IMAGE_SIZE, FV_SIZE))
+
+    tfds.disable_progress_bar() # it is not necessary
+
+
+    # entire_set, info = tfds.load(
+    #     'cats_vs_dogs',
+    #     with_info=True,
+    #     as_supervised=True,
+    # )
+    ds = tfds.load('cats_vs_dogs', split='train', as_supervised=True)
+    mds = []
+    good_count = bad_count = 0
+
+    for image, label in ds.take(1):
+        try:
+            plt.imshow(image)
+            # plt.axis('off')
+            # plt.title("label: " + str(label))
+            # plt.show()
+            element = (image, label)
+            mds.append(element)
+            good_count += 1
+            print('good count: ' + str(good_count))
+        except:
+            bad_count += 1
+    print('len mds is: ' + len(mds))
+    print('bad images = ' + str(bad_count))
+
+
+
+    """
+    # it'll divide dataset in ratio 80%, 10%, 10% (train, validation, test)
+    (train_examples, validation_examples, test_examples), info = tfds.load(
+        'cats_vs_dogs',
+        split=['train[80%:]', 'train[80%:90%]', 'train[90%:]'],
+        with_info=True,
+        as_supervised=True,
+    )
+
+    num_examples = info.splits['train'].num_examples
+    num_classes = info.features['label'].num_classes
+
+    #  print(str(info))  // A large set of images of cats and dogs. There are 1738 corrupted images that are dropped.
+
+    # train_batches = train_examples.shuffle(num_examples // 4).map(format_image).batch(BATCH_SIZE).prefetch(1)
+    # validation_batches = validation_examples.map(format_image).batch(BATCH_SIZE).prefetch(1)
+    # test_batches = test_examples.map(format_image).batch(1)
+
+    """
+
+
+
+
+
+
+
+
+    """
     # Create a simple Keras model.
     x = [-1, 0, 1, 2, 3, 4]
     y = [-3, -1, 1, 3, 5, 7]   # y = 2x - 1
@@ -73,4 +152,5 @@ if __name__ == '__main__':
     plt.title("TFLite model")
     plt.plot(inputs, tflite_res, 'g')
     plt.show()
+    """
 
