3024156f2c42a42081cb9691fe211ee349b3c66f
vitaly.gusev <vitaly.gusev@progforce.com>
2022-03-25 20:33:46 +0300

    release 0.3 The created and saved data set occupies about 15 GB, although the original images are only about 500 MB !!!

--- main.py
+++ main.py
@@ -11,14 +11,91 @@ import os
 import tensorflow_hub as hub
 import tensorflow_datasets as tfds
 import cv2
+import tempfile
 
 
 def format_image(image, label):
   image = tf.image.resize(image, IMAGE_SIZE) / 255.0
   return  image, label
 
+def remove_badlyEncoded_images():
+    num_skipped = 0
+    for folder_name in ("cats_dataset/", "dogs_dataset/"):
+        folder_path = os.path.join('/home/progforce/tensorflow_datasets/', folder_name)
+        for fname in os.listdir(folder_path):
+            fpath = os.path.join(folder_path, fname)
+            try:
+                fobj = open(fpath, "rb")
+                is_jfif = tf.compat.as_bytes("JFIF") in fobj.peek(10) # https://keras.io/examples/vision/image_classification_from_scratch/
+            finally:
+                fobj.close()
 
+            if not is_jfif:
+                num_skipped += 1
+                # Delete corrupted image
+                os.remove(fpath)
 
+    print("Deleted %d images" % num_skipped)
+
+def transformationTFDS_in_ImageDirs():
+    ds = tfds.load('cats_vs_dogs', split='train', as_supervised=True)
+    good_count = bad_count = cats = dogs = 0
+    cats_fls = '/home/progforce/tensorflow_datasets/createDataSet1/cats_dataset/'
+    dogs_fls = '/home/progforce/tensorflow_datasets/createDataSet1/dogs_dataset/'
+
+    for image, label in ds:
+        try:
+            plt.imshow(image)
+            # plt.axis('off')
+            # plt.title("label: " + str(label))
+            # plt.show()
+            if label == 0:
+                cats += 1
+                plt.savefig(cats_fls + "cats_" + str(cats) + ".jpg")  # save the figure to file
+                # with open(cats_fls + "cats_" + str(cats) + ".jpg", "wb") as cat_ds:
+                #     cat_ds.write(image)
+            else:
+                dogs += 1
+                plt.savefig(dogs_fls + "dogs_" + str(cats) + ".jpg")  # save the figure to file
+                # with open(dogs_fls + "dogs_" + str(dogs) +".jpg", "wb") as dog_ds:
+                #     dog_ds.write(image)
+
+            good_count += 1
+            print('good count: ' + str(good_count))
+        except:
+            bad_count += 1
+            print('bad count' + str(bad_count))
+        plt.clf()
+    print('cats =: ' + str(cats))
+    print('dogs =: ' + str(dogs))
+    print('bad images = ' + str(bad_count))
+
+def generate_dataset():
+    image_size = (pixels, pixels)
+    batch_size = BATCH_SIZE
+
+    train_ds = tf.keras.preprocessing.image_dataset_from_directory(
+        "/home/progforce/tensorflow_datasets/createDataSet1/",
+        validation_split=0.2,
+        subset="training",
+        seed=13,
+        image_size=image_size,
+        batch_size=batch_size,
+    )
+    val_ds = tf.keras.preprocessing.image_dataset_from_directory(
+        "/home/progforce/tensorflow_datasets/createDataSet1/",
+        validation_split=0.2,
+        subset="validation",
+        seed=13,
+        image_size=image_size,
+        batch_size=batch_size,
+    )
+
+    # The saved data set occupies about 15 GB, although the original images are only about 500 MB !!!
+    # path_train_ds = os.path.join(tempfile.gettempdir(), "saved_trainDS")
+    # path_valid_ds = os.path.join(tempfile.gettempdir(), "saved_validDS")
+    # tf.data.experimental.save(train_ds, path_train_ds)
+    # tf.data.experimental.save(train_ds, path_valid_ds)
 
 
 if __name__ == '__main__':
@@ -37,42 +114,26 @@ if __name__ == '__main__':
 
     tfds.disable_progress_bar() # it is not necessary
 
+    generate_dataset()
+
+    # new_dataset = tf.data.experimental.load(path)
+    # for elem, label in new_dataset:
+    #     plt.imshow(elem)
+    #     plt.axis('off')
+    #     plt.title("label: " + str(label))
+    #     plt.show()
 
     # entire_set, info = tfds.load(
     #     'cats_vs_dogs',
     #     with_info=True,
     #     as_supervised=True,
     # )
-    ds = tfds.load('cats_vs_dogs', split='train', as_supervised=True)
-    good_count = bad_count = cats = dogs = 0
-    cats_fls = '/home/progforce/tensorflow_datasets/cats_dataset/'
-    dogs_fls = '/home/progforce/tensorflow_datasets/dogs_dataset/'
 
 
-    for image, label in ds:
-        try:
-            plt.imshow(image)
-            # plt.axis('off')
-            # plt.title("label: " + str(label))
-            # plt.show()
-            if label ==0:
-                cats += 1
-                with open(cats_fls + "cats_" + str(cats) + ".jpg", "wb") as cat_ds:
-                    cat_ds.write(image)
 
-            else:
-                dogs += 1
-                with open(dogs_fls + "dogs_" + str(dogs) +".jpg", "wb") as dog_ds:
-                    dog_ds.write(image)
 
-            good_count += 1
-            print('good count: ' + str(good_count))
-        except:
-            bad_count += 1
-            print('bad count' + str(bad_count))
-    print('cats =: ' + str(cats))
-    print('dogs =: ' + str(dogs))
-    print('bad images = ' + str(bad_count))
+
+
 
 
 
